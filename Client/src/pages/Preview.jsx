f7db8f82afbdf458a146b93649b3f0c24e963acf
Komal Godbole <komalgodbole16@gmail.com>
2026-07-15 12:47:47 +0530

    fix isLoading variable naming

--- Client/src/pages/Preview.jsx
+++ Client/src/pages/Preview.jsx
@@ -19,7 +19,7 @@ const Preview = () => {
     } catch (error) {
       console.log(error.message);
     }finally{
-      setIsLoading(false)
+      setisLoading(false)
     }
   }
 
@@ -34,7 +34,7 @@ const Preview = () => {
     </div>
   ) : (
     <div>
-      {IsLoading ? <Loader /> : (
+      {isLoading ? <Loader /> : (
         <div className='flex flex-col items-center justify-center h-screen'>
           <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found</p>
           <a href='/' className='mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors'>
