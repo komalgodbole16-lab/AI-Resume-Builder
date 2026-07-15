bccd4afe20ded2bf2ad395e072317ba210f3c697
Komal Godbole <komalgodbole16@gmail.com>
2026-07-15 12:34:22 +0530

    fix isLoading conditional render in Preview.jsx

--- Client/src/pages/Preview.jsx
+++ Client/src/pages/Preview.jsx
@@ -34,7 +34,7 @@ const Preview = () => {
     </div>
   ) : (
     <div>
-      (IsLoading ? <Loader /> : (
+      {IsLoading ? <Loader /> : (
         <div className='flex flex-col items-center justify-center h-screen'>
           <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found</p>
           <a href='/' className='mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors'>
@@ -42,7 +42,7 @@ const Preview = () => {
             go to home page
           </a>
         </div>
-      ))
+      )}
       
     </div>
   )
